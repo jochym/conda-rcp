6f292db9b45c12bec696f9cfba4665ff088be9cc
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2016-03-07 12:57:57 +0100

    ASE: clean up build data.

--- python-ase/build.sh
+++ python-ase/build.sh
@@ -5,9 +5,3 @@ git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^v//g' > __conda
 #git apply -v /home/anaconda/source/python-ase/*.patch
 
 $PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
