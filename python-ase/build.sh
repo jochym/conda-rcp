d56f0561c890fd13b126c060c1c641197ce2761a
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2016-03-07 12:48:33 +0100

    ASE: Drop patch application from build.sh.

--- python-ase/build.sh
+++ python-ase/build.sh
@@ -2,7 +2,7 @@
 
 git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^v//g' > __conda_version__.txt
 
-git apply -v /home/anaconda/source/python-ase/*.patch
+#git apply -v /home/anaconda/source/python-ase/*.patch
 
 $PYTHON setup.py install
 
